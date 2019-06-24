a35d00d75f32c68828113ecb808ce8618b07be70
yuekcc <yuekcc@qq.com>
2019-06-24 20:51:47 +0800

    更新依赖

--- dist/modalr.esm.js
+++ dist/modalr.esm.js
@@ -1,301 +1,280 @@
-function noop() {}
-
+function noop() { }
 function run(fn) {
-	return fn();
+    return fn();
 }
-
 function blank_object() {
-	return Object.create(null);
+    return Object.create(null);
 }
-
 function run_all(fns) {
-	fns.forEach(run);
+    fns.forEach(run);
 }
-
 function is_function(thing) {
-	return typeof thing === 'function';
+    return typeof thing === 'function';
 }
-
 function safe_not_equal(a, b) {
-	return a != a ? b == b : a !== b || ((a && typeof a === 'object') || typeof a === 'function');
+    return a != a ? b == b : a !== b || ((a && typeof a === 'object') || typeof a === 'function');
 }
 
 function append(target, node) {
-	target.appendChild(node);
+    target.appendChild(node);
 }
-
 function insert(target, node, anchor) {
-	target.insertBefore(node, anchor);
+    target.insertBefore(node, anchor || null);
 }
-
 function detach(node) {
-	node.parentNode.removeChild(node);
+    node.parentNode.removeChild(node);
 }
-
 function element(name) {
-	return document.createElement(name);
+    return document.createElement(name);
 }
-
 function listen(node, event, handler, options) {
-	node.addEventListener(event, handler, options);
-	return () => node.removeEventListener(event, handler, options);
+    node.addEventListener(event, handler, options);
+    return () => node.removeEventListener(event, handler, options);
 }
-
+function attr(node, attribute, value) {
+    if (value == null)
+        node.removeAttribute(attribute);
+    else
+        node.setAttribute(attribute, value);
+}
 function children(element) {
-	return Array.from(element.childNodes);
+    return Array.from(element.childNodes);
 }
-
 function set_style(node, key, value) {
-	node.style.setProperty(key, value);
+    node.style.setProperty(key, value);
 }
-
 function custom_event(type, detail) {
-	const e = document.createEvent('CustomEvent');
-	e.initCustomEvent(type, false, false, detail);
-	return e;
+    const e = document.createEvent('CustomEvent');
+    e.initCustomEvent(type, false, false, detail);
+    return e;
 }
 
 let current_component;
-
 function set_current_component(component) {
-	current_component = component;
+    current_component = component;
 }
-
 function get_current_component() {
-	if (!current_component) throw new Error(`Function called outside component initialization`);
-	return current_component;
+    if (!current_component)
+        throw new Error(`Function called outside component initialization`);
+    return current_component;
 }
-
 function onMount(fn) {
-	get_current_component().$$.on_mount.push(fn);
+    get_current_component().$$.on_mount.push(fn);
 }
-
 function createEventDispatcher() {
-	const component = current_component;
-
-	return (type, detail) => {
-		const callbacks = component.$$.callbacks[type];
-
-		if (callbacks) {
-			// TODO are there situations where events could be dispatched
-			// in a server (non-DOM) environment?
-			const event = custom_event(type, detail);
-			callbacks.slice().forEach(fn => {
-				fn.call(component, event);
-			});
-		}
-	};
+    const component = current_component;
+    return (type, detail) => {
+        const callbacks = component.$$.callbacks[type];
+        if (callbacks) {
+            // TODO are there situations where events could be dispatched
+            // in a server (non-DOM) environment?
+            const event = custom_event(type, detail);
+            callbacks.slice().forEach(fn => {
+                fn.call(component, event);
+            });
+        }
+    };
 }
 
 const dirty_components = [];
-
 const resolved_promise = Promise.resolve();
 let update_scheduled = false;
 const binding_callbacks = [];
 const render_callbacks = [];
 const flush_callbacks = [];
-
 function schedule_update() {
-	if (!update_scheduled) {
-		update_scheduled = true;
-		resolved_promise.then(flush);
-	}
+    if (!update_scheduled) {
+        update_scheduled = true;
+        resolved_promise.then(flush);
+    }
 }
-
 function add_render_callback(fn) {
-	render_callbacks.push(fn);
+    render_callbacks.push(fn);
 }
-
 function flush() {
-	const seen_callbacks = new Set();
-
-	do {
-		// first, call beforeUpdate functions
-		// and update components
-		while (dirty_components.length) {
-			const component = dirty_components.shift();
-			set_current_component(component);
-			update(component.$$);
-		}
-
-		while (binding_callbacks.length) binding_callbacks.shift()();
-
-		// then, once components are updated, call
-		// afterUpdate functions. This may cause
-		// subsequent updates...
-		while (render_callbacks.length) {
-			const callback = render_callbacks.pop();
-			if (!seen_callbacks.has(callback)) {
-				callback();
-
-				// ...so guard against infinite loops
-				seen_callbacks.add(callback);
-			}
-		}
-	} while (dirty_components.length);
-
-	while (flush_callbacks.length) {
-		flush_callbacks.pop()();
-	}
-
-	update_scheduled = false;
+    const seen_callbacks = new Set();
+    do {
+        // first, call beforeUpdate functions
+        // and update components
+        while (dirty_components.length) {
+            const component = dirty_components.shift();
+            set_current_component(component);
+            update(component.$$);
+        }
+        while (binding_callbacks.length)
+            binding_callbacks.shift()();
+        // then, once components are updated, call
+        // afterUpdate functions. This may cause
+        // subsequent updates...
+        while (render_callbacks.length) {
+            const callback = render_callbacks.pop();
+            if (!seen_callbacks.has(callback)) {
+                callback();
+                // ...so guard against infinite loops
+                seen_callbacks.add(callback);
+            }
+        }
+    } while (dirty_components.length);
+    while (flush_callbacks.length) {
+        flush_callbacks.pop()();
+    }
+    update_scheduled = false;
 }
-
 function update($$) {
-	if ($$.fragment) {
-		$$.update($$.dirty);
-		run_all($$.before_render);
-		$$.fragment.p($$.dirty, $$.ctx);
-		$$.dirty = null;
-
-		$$.after_render.forEach(add_render_callback);
-	}
+    if ($$.fragment) {
+        $$.update($$.dirty);
+        run_all($$.before_render);
+        $$.fragment.p($$.dirty, $$.ctx);
+        $$.dirty = null;
+        $$.after_render.forEach(add_render_callback);
+    }
 }
-
+const outroing = new Set();
 let outros;
-
 function group_outros() {
-	outros = {
-		remaining: 0,
-		callbacks: []
-	};
+    outros = {
+        remaining: 0,
+        callbacks: []
+    };
 }
-
 function check_outros() {
-	if (!outros.remaining) {
-		run_all(outros.callbacks);
-	}
+    if (!outros.remaining) {
+        run_all(outros.callbacks);
+    }
 }
-
-function on_outro(callback) {
-	outros.callbacks.push(callback);
+function transition_in(block, local) {
+    if (block && block.i) {
+        outroing.delete(block);
+        block.i(local);
+    }
 }
-
+function transition_out(block, local, callback) {
+    if (block && block.o) {
+        if (outroing.has(block))
+            return;
+        outroing.add(block);
+        outros.callbacks.push(() => {
+            outroing.delete(block);
+            if (callback) {
+                block.d(1);
+                callback();
+            }
+        });
+        block.o(local);
+    }
+}
 function mount_component(component, target, anchor) {
-	const { fragment, on_mount, on_destroy, after_render } = component.$$;
-
-	fragment.m(target, anchor);
-
-	// onMount happens after the initial afterUpdate. Because
-	// afterUpdate callbacks happen in reverse order (inner first)
-	// we schedule onMount callbacks before afterUpdate callbacks
-	add_render_callback(() => {
-		const new_on_destroy = on_mount.map(run).filter(is_function);
-		if (on_destroy) {
-			on_destroy.push(...new_on_destroy);
-		} else {
-			// Edge case - component was destroyed immediately,
-			// most likely as a result of a binding initialising
-			run_all(new_on_destroy);
-		}
-		component.$$.on_mount = [];
-	});
-
-	after_render.forEach(add_render_callback);
+    const { fragment, on_mount, on_destroy, after_render } = component.$$;
+    fragment.m(target, anchor);
+    // onMount happens after the initial afterUpdate. Because
+    // afterUpdate callbacks happen in reverse order (inner first)
+    // we schedule onMount callbacks before afterUpdate callbacks
+    add_render_callback(() => {
+        const new_on_destroy = on_mount.map(run).filter(is_function);
+        if (on_destroy) {
+            on_destroy.push(...new_on_destroy);
+        }
+        else {
+            // Edge case - component was destroyed immediately,
+            // most likely as a result of a binding initialising
+            run_all(new_on_destroy);
+        }
+        component.$$.on_mount = [];
+    });
+    after_render.forEach(add_render_callback);
 }
-
-function destroy(component, detaching) {
-	if (component.$$) {
-		run_all(component.$$.on_destroy);
-		component.$$.fragment.d(detaching);
-
-		// TODO null out other refs, including component.$$ (but need to
-		// preserve final state?)
-		component.$$.on_destroy = component.$$.fragment = null;
-		component.$$.ctx = {};
-	}
+function destroy_component(component, detaching) {
+    if (component.$$.fragment) {
+        run_all(component.$$.on_destroy);
+        if (detaching)
+            component.$$.fragment.d(1);
+        // TODO null out other refs, including component.$$ (but need to
+        // preserve final state?)
+        component.$$.on_destroy = component.$$.fragment = null;
+        component.$$.ctx = {};
+    }
 }
-
 function make_dirty(component, key) {
-	if (!component.$$.dirty) {
-		dirty_components.push(component);
-		schedule_update();
-		component.$$.dirty = {};
-	}
-	component.$$.dirty[key] = true;
+    if (!component.$$.dirty) {
+        dirty_components.push(component);
+        schedule_update();
+        component.$$.dirty = blank_object();
+    }
+    component.$$.dirty[key] = true;
 }
-
 function init(component, options, instance, create_fragment, not_equal$$1, prop_names) {
-	const parent_component = current_component;
-	set_current_component(component);
-
-	const props = options.props || {};
-
-	const $$ = component.$$ = {
-		fragment: null,
-		ctx: null,
-
-		// state
-		props: prop_names,
-		update: noop,
-		not_equal: not_equal$$1,
-		bound: blank_object(),
-
-		// lifecycle
-		on_mount: [],
-		on_destroy: [],
-		before_render: [],
-		after_render: [],
-		context: new Map(parent_component ? parent_component.$$.context : []),
-
-		// everything else
-		callbacks: blank_object(),
-		dirty: null
-	};
-
-	let ready = false;
-
-	$$.ctx = instance
-		? instance(component, props, (key, value) => {
-			if ($$.ctx && not_equal$$1($$.ctx[key], $$.ctx[key] = value)) {
-				if ($$.bound[key]) $$.bound[key](value);
-				if (ready) make_dirty(component, key);
-			}
-		})
-		: props;
-
-	$$.update();
-	ready = true;
-	run_all($$.before_render);
-	$$.fragment = create_fragment($$.ctx);
-
-	if (options.target) {
-		if (options.hydrate) {
-			$$.fragment.l(children(options.target));
-		} else {
-			$$.fragment.c();
-		}
-
-		if (options.intro && component.$$.fragment.i) component.$$.fragment.i();
-		mount_component(component, options.target, options.anchor);
-		flush();
-	}
-
-	set_current_component(parent_component);
+    const parent_component = current_component;
+    set_current_component(component);
+    const props = options.props || {};
+    const $$ = component.$$ = {
+        fragment: null,
+        ctx: null,
+        // state
+        props: prop_names,
+        update: noop,
+        not_equal: not_equal$$1,
+        bound: blank_object(),
+        // lifecycle
+        on_mount: [],
+        on_destroy: [],
+        before_render: [],
+        after_render: [],
+        context: new Map(parent_component ? parent_component.$$.context : []),
+        // everything else
+        callbacks: blank_object(),
+        dirty: null
+    };
+    let ready = false;
+    $$.ctx = instance
+        ? instance(component, props, (key, value) => {
+            if ($$.ctx && not_equal$$1($$.ctx[key], $$.ctx[key] = value)) {
+                if ($$.bound[key])
+                    $$.bound[key](value);
+                if (ready)
+                    make_dirty(component, key);
+            }
+        })
+        : props;
+    $$.update();
+    ready = true;
+    run_all($$.before_render);
+    $$.fragment = create_fragment($$.ctx);
+    if (options.target) {
+        if (options.hydrate) {
+            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+            $$.fragment.l(children(options.target));
+        }
+        else {
+            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+            $$.fragment.c();
+        }
+        if (options.intro)
+            transition_in(component.$$.fragment);
+        mount_component(component, options.target, options.anchor);
+        flush();
+    }
+    set_current_component(parent_component);
 }
-
 class SvelteComponent {
-	$destroy() {
-		destroy(this, true);
-		this.$destroy = noop;
-	}
-
-	$on(type, callback) {
-		const callbacks = (this.$$.callbacks[type] || (this.$$.callbacks[type] = []));
-		callbacks.push(callback);
-
-		return () => {
-			const index = callbacks.indexOf(callback);
-			if (index !== -1) callbacks.splice(index, 1);
-		};
-	}
-
-	$set() {
-		// overridden by instance, if it has props
-	}
+    $destroy() {
+        destroy_component(this, 1);
+        this.$destroy = noop;
+    }
+    $on(type, callback) {
+        const callbacks = (this.$$.callbacks[type] || (this.$$.callbacks[type] = []));
+        callbacks.push(callback);
+        return () => {
+            const index = callbacks.indexOf(callback);
+            if (index !== -1)
+                callbacks.splice(index, 1);
+        };
+    }
+    $set() {
+        // overridden by instance, if it has props
+    }
 }
 
-/* src\Loading.svelte generated by Svelte v3.1.0 */
+/* src\Loading.svelte generated by Svelte v3.5.4 */
 
 function add_css() {
 	var style = element("style");
@@ -311,10 +290,10 @@ function create_fragment(ctx) {
 		c() {
 			div = element("div");
 			img = element("img");
-			img.src = ctx.LoadingImage;
-			img.alt = "加载中";
-			img.className = "modalr-loading-img svelte-1m5zham";
-			div.className = "modalr-loading svelte-1m5zham";
+			attr(img, "src", ctx.LoadingImage);
+			attr(img, "alt", "加载中");
+			attr(img, "class", "modalr-loading-img svelte-1m5zham");
+			attr(div, "class", "modalr-loading svelte-1m5zham");
 		},
 
 		m(target, anchor) {
@@ -358,7 +337,7 @@ const createZIndexManager = () => {
 
 const nextIndex = createZIndexManager();
 
-/* src\ModalContainer.svelte generated by Svelte v3.1.0 */
+/* src\ModalContainer.svelte generated by Svelte v3.5.4 */
 
 function add_css$1() {
 	var style = element("style");
@@ -374,8 +353,8 @@ function create_else_block(ctx) {
 	return {
 		c() {
 			div = element("div");
-			div.id = ctx.contentWrapperId;
-			div.className = "flex-item svelte-1nsld77";
+			attr(div, "id", ctx.contentWrapperId);
+			attr(div, "class", "flex-item svelte-1nsld77");
 			set_style(div, "z-index", ctx.clildZIndex);
 		},
 
@@ -390,7 +369,7 @@ function create_else_block(ctx) {
 			}
 
 			if (changed.contentWrapperId) {
-				div.id = ctx.contentWrapperId;
+				attr(div, "id", ctx.contentWrapperId);
 			}
 
 			if (changed.clildZIndex) {
@@ -419,7 +398,7 @@ function create_if_block(ctx) {
 		c() {
 			div = element("div");
 			loading.$$.fragment.c();
-			div.className = "flex-item svelte-1nsld77";
+			attr(div, "class", "flex-item svelte-1nsld77");
 			set_style(div, "z-index", ctx.clildZIndex);
 		},
 
@@ -437,13 +416,13 @@ function create_if_block(ctx) {
 
 		i(local) {
 			if (current) return;
-			loading.$$.fragment.i(local);
+			transition_in(loading.$$.fragment, local);
 
 			current = true;
 		},
 
 		o(local) {
-			loading.$$.fragment.o(local);
+			transition_out(loading.$$.fragment, local);
 			current = false;
 		},
 
@@ -452,7 +431,7 @@ function create_if_block(ctx) {
 				detach(div);
 			}
 
-			loading.$destroy();
+			destroy_component(loading, );
 		}
 	};
 }
@@ -480,7 +459,7 @@ function create_fragment$1(ctx) {
 			div = element("div");
 			if_block.c();
 			div.dataset.modalrId = ctx.id;
-			div.className = "modalr-dialog-container modalr-flex-container svelte-1nsld77";
+			attr(div, "class", "modalr-dialog-container modalr-flex-container svelte-1nsld77");
 			set_style(div, "z-index", ctx.continerZIndex);
 			set_style(div, "background-color", ctx.backgroundColor);
 			dispose = listen(div, "click", ctx.handleModalContainerOnClick);
@@ -499,11 +478,9 @@ function create_fragment$1(ctx) {
 				if_blocks[current_block_type_index].p(changed, ctx);
 			} else {
 				group_outros();
-				on_outro(() => {
-					if_blocks[previous_block_index].d(1);
+				transition_out(if_blocks[previous_block_index], 1, () => {
 					if_blocks[previous_block_index] = null;
 				});
-				if_block.o(1);
 				check_outros();
 
 				if_block = if_blocks[current_block_type_index];
@@ -511,7 +488,7 @@ function create_fragment$1(ctx) {
 					if_block = if_blocks[current_block_type_index] = if_block_creators[current_block_type_index](ctx);
 					if_block.c();
 				}
-				if_block.i(1);
+				transition_in(if_block, 1);
 				if_block.m(div, null);
 			}
 
@@ -530,12 +507,12 @@ function create_fragment$1(ctx) {
 
 		i(local) {
 			if (current) return;
-			if (if_block) if_block.i();
+			transition_in(if_block);
 			current = true;
 		},
 
 		o(local) {
-			if (if_block) if_block.o();
+			transition_out(if_block);
 			current = false;
 		},
 
@@ -586,6 +563,7 @@ function instance$1($$self, $$props, $$invalidate) {
 	};
 
 	let continerZIndex, clildZIndex, backgroundColor, contentWrapperId, isLoading;
+
 	$$self.$$.update = ($$dirty = { config: 1, id: 1 }) => {
 		if ($$dirty.config) { $$invalidate('continerZIndex', continerZIndex = config.zindex); }
 		if ($$dirty.config) { $$invalidate('clildZIndex', clildZIndex = config.zindex + 10); }
@@ -693,6 +671,9 @@ var modalr = {
     }
   },
 
+  /**
+   * 关闭最后一个弹出层
+   */
   closeLatest() {
     const lastId = dialogIds.pop();
     this.close(lastId);
